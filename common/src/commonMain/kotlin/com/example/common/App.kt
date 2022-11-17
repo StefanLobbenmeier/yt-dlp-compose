52c3cf1d266e7d9c152afe8725d87a50cd0a803c
Stefan Lobbenmeier <stefan.lobbenmeier@objego.de>
2022-11-17 23:06:00 +0100

    Use Scaffold for the layout

--- common/src/commonMain/kotlin/com/example/common/App.kt
+++ common/src/commonMain/kotlin/com/example/common/App.kt
@@ -2,6 +2,7 @@ package com.example.common
 
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
@@ -14,16 +15,15 @@ import androidx.compose.ui.unit.dp
 
 @Composable
 fun App() {
-    Column(modifier = Modifier.padding(8.0.dp)) {
-        Header()
-        DownloadList()
-        Footer()
-    }
+    Scaffold (
+        topBar = { Header() },
+        bottomBar = { Footer() }
+    ) { DownloadList() }
 }
 
 @Composable
 fun Header() {
-    Row(horizontalArrangement = Arrangement.Center, modifier = Modifier.padding(8.0.dp)) {
+    Row(horizontalArrangement = Arrangement.Center) {
         var downloadUrl by remember { mutableStateOf("") }
         TextField(downloadUrl, maxLines = 1, onValueChange = {downloadUrl = it},
                 modifier = Modifier.weight(1F).fillMaxWidth().padding(8.dp),)
@@ -35,7 +35,7 @@ fun DownloadList() {
     val downloadList = List(20) {
         "Download Item $it"
     }
-    LazyColumn (modifier = Modifier.padding(8.0.dp)) {
+    LazyColumn (modifier = Modifier.padding(8.0.dp).fillMaxSize()) {
         downloadList.forEach {
             item { Text(it) }
         }
