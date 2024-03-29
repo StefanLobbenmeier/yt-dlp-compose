04cd1d8e63761a88f76858fa37e65adc30078e5d
Stefan Lobbenmeier <stefan.lobbenmeier@gmail.com>
2024-03-29 13:11:01 -0300

    add test that actually tests the concurrent execution

--- src/test/kotlin/de/lobbenmeier/stefan/common/business/DynamicSemaphoreTest.kt
+++ src/test/kotlin/de/lobbenmeier/stefan/common/business/DynamicSemaphoreTest.kt
@@ -2,6 +2,11 @@ package de.lobbenmeier.stefan.common.business
 
 import io.kotest.core.spec.style.AnnotationSpec
 import io.kotest.matchers.shouldBe
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.async
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withPermit
+import kotlinx.coroutines.test.advanceUntilIdle
 import kotlinx.coroutines.test.runTest
 
 class DynamicSemaphoreTest : AnnotationSpec() {
@@ -46,5 +51,75 @@ class DynamicSemaphoreTest : AnnotationSpec() {
         // 1
         sut.release()
         sut.tryAcquire() shouldBe true
+    }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    fun testUpdatePermits_concurrentTest() = runTest {
+        val sut = DynamicSemaphore(1)
+
+        val taskWaiter = Mutex(locked = true)
+
+        var task1Started = false
+        var task2Started = false
+        var task3Started = false
+
+        async {
+            sut.withPermit {
+                task1Started = true
+                taskWaiter.lock()
+            }
+        }
+        advanceUntilIdle()
+
+        async {
+            sut.withPermit {
+                task2Started = true
+                taskWaiter.lock()
+            }
+        }
+        advanceUntilIdle()
+
+        async {
+            sut.withPermit {
+                task3Started = true
+                taskWaiter.lock()
+            }
+        }
+        advanceUntilIdle()
+
+        task1Started shouldBe true
+        task2Started shouldBe false
+        task3Started shouldBe false
+
+        sut.updatePermits(2)
+
+        advanceUntilIdle()
+
+        task2Started shouldBe true
+        task3Started shouldBe false
+
+        sut.updatePermits(1)
+        advanceUntilIdle()
+
+        task3Started shouldBe false
+
+        // finish task 1
+        taskWaiter.unlock()
+        advanceUntilIdle()
+
+        task3Started shouldBe false
+
+        // finish task 2
+        taskWaiter.unlock()
+        advanceUntilIdle()
+
+        task3Started shouldBe true
+
+        // complete test
+        taskWaiter.unlock()
+        advanceUntilIdle()
+
+        sut.availablePermits shouldBe 1
     }
 }
