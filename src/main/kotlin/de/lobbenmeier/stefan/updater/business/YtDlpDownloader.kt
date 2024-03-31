4ebc9e0fd8ae34d43e3e15f2eb44e7dd3e62a508
Stefan Lobbenmeier <stefan.lobbenmeier@gmail.com>
2024-03-31 11:58:13 -0300

    switch to yt-dlp nightly builds, since reporting errors to yt-dlp requires this

--- src/main/kotlin/de/lobbenmeier/stefan/updater/business/YtDlpDownloader.kt
+++ src/main/kotlin/de/lobbenmeier/stefan/updater/business/YtDlpDownloader.kt
@@ -4,4 +4,4 @@ import de.lobbenmeier.stefan.updater.business.github.GithubReleaseDownloader
 import java.nio.file.Path
 
 fun createYtDlpDownloader(downloadDirectory: Path) =
-    GithubReleaseDownloader("yt-dlp", "yt-dlp", downloadDirectory)
+    GithubReleaseDownloader("yt-dlp", "yt-dlp-nightly-builds", downloadDirectory)
