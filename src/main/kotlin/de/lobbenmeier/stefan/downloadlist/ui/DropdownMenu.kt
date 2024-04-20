15c25001e9e86d2e4fbace2f525105945595f7c9
Stefan Lobbenmeier <stefan.lobbenmeier@gmail.com>
2024-04-20 21:14:23 +0200

    Reuse DropdownMenu for SettingsUI

--- src/main/kotlin/de/lobbenmeier/stefan/downloadlist/ui/DropdownMenu.kt
+++ src/main/kotlin/de/lobbenmeier/stefan/downloadlist/ui/DropdownMenu.kt
@@ -1,6 +1,5 @@
 package de.lobbenmeier.stefan.downloadlist.ui
 
-import androidx.compose.foundation.layout.width
 import androidx.compose.material.DropdownMenuItem
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.ExposedDropdownMenuBox
@@ -13,7 +12,6 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.Dp
 
 @OptIn(ExperimentalMaterialApi::class)
 @Composable
@@ -21,8 +19,10 @@ fun <T> DropdownMenu(
     options: List<T>,
     optionFormatter: (T?) -> String = { it.toString() },
     modifier: Modifier = Modifier,
+    textFieldModifier: Modifier = Modifier,
     selectedOption: T?,
     selectionChanged: (T) -> Unit,
+    onTextInput: ((String) -> Unit)? = null,
     label: String? = null,
 ) {
     var expanded by remember { mutableStateOf(false) }
@@ -34,10 +34,11 @@ fun <T> DropdownMenu(
     ) {
         val value: String = optionFormatter(selectedOption)
         OutlinedTextField(
-            readOnly = true,
+            modifier = textFieldModifier,
+            readOnly = onTextInput == null,
             value = value,
-            onValueChange = {},
             label = label?.let { { Text(text = it) } },
+            onValueChange = onTextInput ?: {},
             trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded) },
         )
         ExposedDropdownMenu(expanded = expanded, onDismissRequest = { expanded = false }) {
@@ -49,46 +50,6 @@ fun <T> DropdownMenu(
                     }
                 ) {
                     Text(optionFormatter(selectionOption))
-                }
-            }
-        }
-    }
-}
-
-@OptIn(ExperimentalMaterialApi::class)
-@Composable
-fun SuggestionMenu(
-    options: List<String>,
-    optionBuilder: @Composable (String) -> Unit = { Text(it) },
-    modifier: Modifier = Modifier,
-    selectedOption: String,
-    selectionChanged: (String) -> Unit,
-    label: String,
-    width: Dp? = null,
-) {
-    var expanded by remember { mutableStateOf(false) }
-
-    ExposedDropdownMenuBox(
-        expanded = expanded,
-        onExpandedChange = { expanded = it },
-        modifier = modifier
-    ) {
-        OutlinedTextField(
-            value = selectedOption,
-            onValueChange = selectionChanged,
-            label = { Text(label) },
-            trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded) },
-            modifier = width?.let { Modifier.width(it) } ?: Modifier
-        )
-        ExposedDropdownMenu(expanded = expanded, onDismissRequest = { expanded = false }) {
-            options.forEach { selectionOption ->
-                DropdownMenuItem(
-                    onClick = {
-                        selectionChanged(selectionOption)
-                        expanded = false
-                    }
-                ) {
-                    optionBuilder(selectionOption)
                 }
             }
         }
