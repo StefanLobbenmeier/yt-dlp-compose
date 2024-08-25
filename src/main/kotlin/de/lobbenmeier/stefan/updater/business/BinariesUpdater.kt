dcff47f39f6477eb26ee9f88076f0a1e58087631
Stefan Lobbenmeier <stefan.lobbenmeier@gmail.com>
2024-08-25 20:00:17 +0200

    Now works with DISK

--- src/main/kotlin/de/lobbenmeier/stefan/updater/business/BinariesUpdater.kt
+++ src/main/kotlin/de/lobbenmeier/stefan/updater/business/BinariesUpdater.kt
@@ -5,12 +5,12 @@ import androidx.compose.runtime.snapshots.SnapshotStateList
 import de.lobbenmeier.stefan.downloadlist.business.DownloadStarted
 import de.lobbenmeier.stefan.downloadlist.business.UpdateDownloadProgress
 import de.lobbenmeier.stefan.settings.business.BinariesSettings
+import de.lobbenmeier.stefan.settings.business.FfmpegLocation
 import de.lobbenmeier.stefan.settings.business.Settings
 import de.lobbenmeier.stefan.settings.business.YtDlpLocation
 import de.lobbenmeier.stefan.updater.business.ffmpeg.FfmpegReleaseDownloader
 import de.lobbenmeier.stefan.updater.model.Binaries
 import de.lobbenmeier.stefan.updater.model.BinariesProgress
-import de.lobbenmeier.stefan.updater.model.LocalBinaryProgress
 import de.lobbenmeier.stefan.updater.model.RemoteBinaryProgress
 import java.io.File
 import java.nio.file.Path
@@ -49,50 +49,49 @@ class BinariesUpdater(
     val binaries = MutableStateFlow<Binaries?>(null)
 
     init {
-        progress.add(checkYtDlp())
-        downloadBinaries()
+        downloadOrFindBinaries()
     }
 
-    private fun checkYtDlp(): BinariesProgress {
-        if (binariesSettings.ytDlpSource == YtDlpLocation.DISK) {
-            if (binariesSettings.ytDlpPath != null) {
-                return LocalBinaryProgress("yt-dlp", "found on disk")
-            }
-
-            val detectOnPath = detectOnPath("yt-dlp")
-
-            if (detectOnPath.isNotEmpty()) {
-                return LocalBinaryProgress("yt-dlp", "found on path")
-            }
-        }
-
-        return downloadYtDlp()
-    }
-
-    private fun downloadBinaries() {
-        val ytDlpProcess = downloadYtDlp()
-        val (ffmpegProcess, ffprobeProcess) = downloadFfmpeg()
-        //        progress =
-        //            mutableStateListOf(
-        //                ytDlpProcess,
-        //                ffmpegProcess,
-        //                ffprobeProcess,
-        //            )
+    private fun downloadOrFindBinaries() {
 
         CoroutineScope(Dispatchers.IO).launch {
-            val ytDlpFuture = async {
-                ytDlpDownloader.downloadRelease(
-                    platform.ytDlpName.filename,
-                    withProgress(ytDlpProcess)
-                )
-            }
-            val ffmpegFuture = async {
-                ffmpegReleaseDownloader.downloadRelease(
-                    platform,
-                    withProgress(ffmpegProcess),
-                    withProgress(ffprobeProcess)
-                )
-            }
+            val ytDlpFuture =
+                if (
+                    binariesSettings.ytDlpSource != YtDlpLocation.DISK ||
+                        binariesSettings.ytDlpPath == null
+                ) {
+                    val ytDlpProcess = downloadYtDlp()
+                    async {
+                        ytDlpDownloader.downloadRelease(
+                            platform.ytDlpName.filename,
+                            withProgress(ytDlpProcess)
+                        )
+                    }
+                } else {
+                    async { findBinary(binariesSettings.ytDlpPath, platform.ytDlpName.filename) }
+                }
+
+            val ffmpegFuture =
+                if (
+                    binariesSettings.ffmpegSource != FfmpegLocation.DISK ||
+                        binariesSettings.ffmpegPath == null
+                ) {
+                    async {
+                        val (ffmpegProcess, ffprobeProcess) = downloadFfmpeg()
+                        val binaries =
+                            ffmpegReleaseDownloader.downloadRelease(
+                                platform,
+                                withProgress(ffmpegProcess),
+                                withProgress(ffprobeProcess)
+                            )
+                        binaries.first()
+                    }
+                } else {
+                    async {
+                        // yt-dlp will resolve for us
+                        File(binariesSettings.ffmpegPath)
+                    }
+                }
 
             val ytDlp = ytDlpFuture.await()
             val ffmpeg = ffmpegFuture.await()
@@ -100,8 +99,7 @@ class BinariesUpdater(
             binaries.value =
                 Binaries(
                     ytDlp.toPath(),
-                    ffmpeg.first().toPath(),
-                    ffmpeg[1].toPath(),
+                    ffmpeg.toPath(),
                 )
         }
     }
@@ -114,6 +112,15 @@ class BinariesUpdater(
 
     private fun downloadYtDlp(): RemoteBinaryProgress {
         return updateProcess(platform.ytDlpName.filename)
+    }
+
+    private fun findBinary(path: String, binary: String): File {
+        val ytDlpPathAsFile = File(path)
+        if (ytDlpPathAsFile.isFile()) {
+            return ytDlpPathAsFile
+        }
+
+        return ytDlpPathAsFile.resolve(binary)
     }
 
     private fun updateProcess(
--- src/main/kotlin/de/lobbenmeier/stefan/updater/model/Binaries.kt
+++ src/main/kotlin/de/lobbenmeier/stefan/updater/model/Binaries.kt
@@ -5,12 +5,10 @@ import java.nio.file.Path
 data class Binaries(
     val ytDlp: Path,
     val ffmpeg: Path,
-    val ffprobe: Path,
 )
 
 val homeBrewBinaries =
     Binaries(
         Path.of("/opt/homebrew/bin/yt-dlp"),
         Path.of("/opt/homebrew/bin/ffmpeg"),
-        Path.of("/opt/homebrew/bin/ffprobe"),
     )
