9a6802493ca6abb39a8e39683ae99e0b79395224
Stefan Lobbenmeier <stefan.lobbenmeier@gmail.com>
2024-12-27 20:49:29 -0300

    Add hot reload manually while I am developing with java hot swap and -XX:+AllowEnhancedClassRedefinition

--- src/main/kotlin/de/lobbenmeier/stefan/downloadlist/ui/DownloadItemPlaylistEntriesView.kt
+++ src/main/kotlin/de/lobbenmeier/stefan/downloadlist/ui/DownloadItemPlaylistEntriesView.kt
@@ -8,15 +8,20 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import de.lobbenmeier.stefan.common.ui.reloadUiEvery
 import de.lobbenmeier.stefan.downloadlist.business.DownloadItem
 import de.lobbenmeier.stefan.downloadlist.business.VideoMetadata
 import de.lobbenmeier.stefan.downloadlist.business.entryThumbnail
+import kotlin.time.Duration.Companion.seconds
 
 @Composable
 fun DownloadItemPlaylistEntriesView(downloadItem: DownloadItem) {
+    reloadUiEvery(1.seconds).value
+
     val metadataNonFinal by downloadItem.metadata.collectAsState()
     val metadata = metadataNonFinal
 
@@ -33,7 +38,10 @@ fun DownloadItemPlaylistEntriesView(downloadItem: DownloadItem) {
 
 @Composable
 fun PlaylistEntryView(entry: VideoMetadata) {
-    Row(Modifier.height(50.dp).padding(all = 8.dp)) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier.height(50.dp).padding(all = 8.dp)
+    ) {
         Thumbnail(entry.entryThumbnail)
         Text(entry.title ?: "No Title", fontWeight = FontWeight.Bold)
     }
