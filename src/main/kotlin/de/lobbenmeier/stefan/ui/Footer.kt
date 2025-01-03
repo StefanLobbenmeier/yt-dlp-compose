11edaaeecd11db3f64a9007ffc6b6934e8fe591a
Stefan Lobbenmeier <stefan.lobbenmeier@gmail.com>
2025-01-02 21:08:10 -0300

    add QuickSetting, which will be used for dialogs for most quick settings

--- src/main/kotlin/de/lobbenmeier/stefan/ui/Footer.kt
+++ src/main/kotlin/de/lobbenmeier/stefan/ui/Footer.kt
@@ -3,9 +3,20 @@ package de.lobbenmeier.stefan.ui
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.material.BottomAppBar
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
+import compose.icons.FeatherIcons
+import compose.icons.feathericons.Key
 import de.lobbenmeier.stefan.settings.business.Settings
 import de.lobbenmeier.stefan.settings.ui.DirectoryPickerButton
 
@@ -15,7 +26,7 @@ fun Footer(settings: Settings, updateSettings: (Settings) -> Unit) {
         Column {
             Row {
                 DownloadFolderSetting(settings, updateSettings)
-                Text("Key")
+                AuthenticationSetting(settings, updateSettings)
                 Text("Subtitles")
                 Text("Spacer")
                 Text("Media")
@@ -31,7 +42,6 @@ fun Footer(settings: Settings, updateSettings: (Settings) -> Unit) {
 
 @Composable
 fun DownloadFolderSetting(settings: Settings, updateSettings: (Settings) -> Unit) {
-
     return DirectoryPickerButton(
         description = "Download Folder",
         value = settings.downloadFolder,
@@ -39,4 +49,29 @@ fun DownloadFolderSetting(settings: Settings, updateSettings: (Settings) -> Unit
     )
 }
 
-@Composable fun QuickSettingDialog() {}
+@Composable
+fun AuthenticationSetting(settings: Settings, updateSettings: (Settings) -> Unit) {
+    return QuickSetting(icon = { Icon(FeatherIcons.Key, contentDescription = "Authentication") }) {}
+}
+
+@OptIn(ExperimentalComposeUiApi::class)
+@Composable
+fun QuickSetting(icon: @Composable () -> Unit, dialogContent: @Composable () -> Unit) {
+    var quickSettingsOpen by remember { mutableStateOf(false) }
+
+    if (quickSettingsOpen) {
+        Dialog(
+            onDismissRequest = { quickSettingsOpen = false },
+            properties =
+                DialogProperties(usePlatformInsets = false, usePlatformDefaultWidth = false),
+        ) {
+            dialogContent()
+        }
+    }
+
+    return IconButton(
+        onClick = { quickSettingsOpen = !quickSettingsOpen },
+    ) {
+        icon()
+    }
+}
