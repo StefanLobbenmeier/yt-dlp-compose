aac9c4c318e76875b047ac36d0eb7a608f222f8c
Stefan Lobbenmeier <stefan.lobbenmeier@gmail.com>
2025-01-14 16:45:13 -0300

    Show CheckForUpdate in main content view

---
+++ src/main/kotlin/de/lobbenmeier/stefan/version/CheckForAppUpdate.kt
@@ -0,0 +1,11 @@
+package de.lobbenmeier.stefan.version
+
+import androidx.compose.material.Card
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun CheckForAppUpdate() {
+
+    Card { Text("Checking for update ...") }
+}
