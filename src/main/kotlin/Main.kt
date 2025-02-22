458115752279c1410d33f5e3437f0272f2c9e8a9
Stefan Lobbenmeier <Stefan.Lobbenmeier@gmail.com>
2025-02-22 17:54:46 +0100

    Improve error message "Failed to start JVM" (#154)

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,24 @@
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import de.lobbenmeier.stefan.ui.App
+import java.awt.Button
+import java.awt.Dialog
+import java.awt.FlowLayout
+import java.awt.Frame
+import java.awt.Label
 
 fun main() {
+    Thread.setDefaultUncaughtExceptionHandler { _, e ->
+        Dialog(Frame(), e.message ?: "Error").apply {
+            layout = FlowLayout()
+            add(Label(e.message))
+            add(Label(e.stackTraceToString()))
+            val button = Button("OK").apply { addActionListener { dispose() } }
+            add(button)
+            setSize(300, 300)
+            isVisible = true
+        }
+    }
+
     application { Window(onCloseRequest = ::exitApplication) { App() } }
 }
