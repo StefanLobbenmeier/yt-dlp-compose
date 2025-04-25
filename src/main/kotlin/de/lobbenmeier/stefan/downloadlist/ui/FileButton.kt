73af2a2933c41d1c1d700f2a62ce795f2a8b47f6
Stefan Lobbenmeier <Stefan.Lobbenmeier@gmail.com>
2025-04-25 19:19:25 +0200

    Fix browse file on Windows and Linux (#176)
    Fixes https://github.com/StefanLobbenmeier/yt-dlp-compose/issues/157

--- src/main/kotlin/de/lobbenmeier/stefan/downloadlist/ui/FileButton.kt
+++ src/main/kotlin/de/lobbenmeier/stefan/downloadlist/ui/FileButton.kt
@@ -2,13 +2,20 @@ package de.lobbenmeier.stefan.downloadlist.ui
 
 import androidx.compose.material.Icon
 import androidx.compose.runtime.Composable
+import com.github.pgreze.process.process
 import compose.icons.FeatherIcons
 import compose.icons.feathericons.Folder
 import compose.icons.feathericons.Play
 import de.lobbenmeier.stefan.common.ui.SmallIconButton
+import de.lobbenmeier.stefan.updater.business.Platform
+import de.lobbenmeier.stefan.updater.business.PlatformType
+import de.lobbenmeier.stefan.updater.business.platform
 import io.github.oshai.kotlinlogging.KotlinLogging
 import java.awt.Desktop
 import java.io.File
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 val logger = KotlinLogging.logger {}
 
@@ -20,7 +27,7 @@ fun OpenFileButton(file: File?) {
         onClick = {
             if (file != null) {
                 if (file.exists()) {
-                    Desktop.getDesktop().open(file)
+                    platform.openFile(file)
                 } else {
                     logger.error { "Could not find file $file" }
                 }
@@ -39,7 +46,7 @@ fun BrowseFileButton(file: File?) {
         onClick = {
             if (file != null) {
                 if (file.exists()) {
-                    Desktop.getDesktop().browseFileDirectory(file.absoluteFile)
+                    platform.browseDirectory(file)
                 } else {
                     logger.error { "Could not find file $file" }
                 }
@@ -48,4 +55,34 @@ fun BrowseFileButton(file: File?) {
     ) {
         Icon(FeatherIcons.Folder, "Show file in folder")
     }
+}
+
+private fun Platform.openFile(file: File) {
+    if (Desktop.getDesktop().isSupported(Desktop.Action.OPEN)) {
+        return Desktop.getDesktop().open(file)
+    }
+
+    val command =
+        when (platformType) {
+            PlatformType.WINDOWS -> arrayOf("explorer.exe", file.absolutePath)
+            PlatformType.MAC_OS -> arrayOf("open", file.absolutePath)
+            PlatformType.LINUX -> arrayOf("xdg-open", file.absolutePath)
+        }
+
+    CoroutineScope(Dispatchers.IO).launch { process(*command) }
+}
+
+private fun Platform.browseDirectory(file: File) {
+    if (Desktop.getDesktop().isSupported(Desktop.Action.BROWSE_FILE_DIR)) {
+        return Desktop.getDesktop().browseFileDirectory(file.absoluteFile)
+    }
+
+    val command =
+        when (platformType) {
+            PlatformType.WINDOWS -> arrayOf("explorer.exe", "/select,", file.absolutePath)
+            PlatformType.MAC_OS -> arrayOf("open", "-R", file.absolutePath)
+            PlatformType.LINUX -> arrayOf("xdg-open", file.parentFile.absolutePath)
+        }
+
+    CoroutineScope(Dispatchers.IO).launch { process(*command) }
 }
